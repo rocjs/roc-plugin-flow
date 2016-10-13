f3d25331eb1755d1db339433b25bc193ce498486
Dan Etherington <dan.etherington@schibsted.com>
2016-10-13 12:34:11 +0100

    better reporting of errors and success

--- extensions/roc-plugin-flowtype/src/actions/flow.js
+++ extensions/roc-plugin-flowtype/src/actions/flow.js
@@ -4,17 +4,25 @@ import flow from 'flow-bin';
 
 const log = initLog();
 
-export default () => (targets) => {
-    log.large.info('Starting flow type check.');
-    
-    return () => execFile(flow, ['check'], (err, stdout) => {
+const HEAVY_EXCLAMATION_MARK_SYMBOL = '❗';
 
+const numErrors = (stdout) => /Found (\d+) error[s]?/.exec(stdout)[1];
+const removeErrorFooter = (stdout) => stdout.replace(/Found \d+ error[s]?/, '');
+const errorString = (num) => num > 1 ? 'errors' : 'error';
+
+export default () => (targets) => {
+    log.small.info('Starting flow type check. This may take a short while, please be patient.\n');
+
+    return () => execFile(flow, ['check'], (err, stdout) => {
         if (err) {
-            log.small.error(err);
+            let num = numErrors(stdout);
+
+            log.small.raw('warn', 'red', HEAVY_EXCLAMATION_MARK_SYMBOL)(`Flow found ${num} ${errorString(num)}:\n`);
+            log.small.raw('error')(removeErrorFooter(stdout));
         }
 
         if (stdout) {
-            log.small.info(stdout);
+            log.small.success(stdout);
         }
     });
 }
